b4ae461cc07a53d681c17d5ebb9670ebbfdd0649
KingdomCross <alexchheng1998@yahoo.com>
2019-09-15 20:05:38 -0700

    Finish

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 #include <stdio.h>
+#include <iostream>
 
 //http://www.cplusplus.com/doc/tutorial/pointers/
 using namespace std;
@@ -17,40 +18,66 @@ int readData(int *arr){
     return *arr;
 }
 
-void bsort(int *arr, int last){
+void bsort(int *arr, int last, int *pointer){
+    cout << "Bubble sort " << endl;
     bool change = true;
     int numChange = 0;
     int i = 0;
     int temp1 = 0;
     int temp2 = 0;
-    while (last > i || change){
-        if (i < last - 1){
-            temp1 = arr[i];
-            temp2 = arr[i + 1];
-            //cout << temp1 << endl << temp2 << endl << endl;
-            if (temp1 > temp2){
-                arr[i + 1] = temp1;
-                arr[i] = temp2;
-                numChange++;
+    if (*pointer == -1){
+        while (last > i || change){
+            if (i < last - 1){
+                temp1 = arr[i];
+                temp2 = arr[i + 1];
+                //cout << temp1 << endl << temp2 << endl << endl;
+                if (temp1 > temp2){
+                    arr[i + 1] = temp1;
+                    arr[i] = temp2;
+                    numChange++;
+                }
+            }
+            i++;
+            if (numChange > 0 && last <= i){
+                i = 0;
+                numChange = 0;
+            }
+            else if (numChange <= 0 && last <= i) {
+                change = false;
             }
         }
-        i++;
-        if (numChange > 0 && last <= i){
-            i = 0;
-            numChange = 0;
+    }
+
+    else if (*pointer == 1){
+        while (last > i || change){
+            if (i < last - 1){
+                temp1 = arr[i];
+                temp2 = arr[i + 1];
+                //cout << temp1 << endl << temp2 << endl << endl;
+                if (temp1 < temp2){
+                    arr[i + 1] = temp1;
+                    arr[i] = temp2;
+                    numChange++;
+                }
+            }
+            i++;
+            if (numChange > 0 && last <= i){
+                i = 0;
+                numChange = 0;
+            }
+            else if (numChange <= 0 && last <= i) {
+                change = false;
+            }
         }
-        else if (numChange <= 0 && last <= i) {
-            change = false;
-        }
+    }
+
+    else {
+        cout << "Error, contact programmer \n";
     }
 }
 
 void writeToConsole(int * arr, int last){
     readData(arr);
-}
-
-void bubble_sort(){
-
 }
 
 int main() {
@@ -69,9 +96,32 @@ int main() {
         textLength++;
     }
 
-    //readData(arrayP);
-    bsort(arrayP, 10);
-    writeToConsole(arrayP, 10);
+    readData(arrayP);
+
+    int select = 0;
+    int n = 0;
+    bool loop = true;
+
+    while (loop){
+        cout << "Enter 0 to exit, else 1\n";
+        cin >> select;
+
+        if (select == 0){
+            loop = false;
+        }
+        else {
+            cout << "Bubble sort, enter 1 ascending order or -1 descending order\n";
+            cin >> n;
+            cout << n << endl;
+            if (n == 1 || n == -1){
+                bsort(arrayP, 10, &n);
+                writeToConsole(arrayP, 10);
+            }
+            else{
+                cout << "Wrong input\n";
+            }
+        }
+    }
 
     inFile.close(); //close file
     return 0;
